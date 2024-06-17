cbec8d411fbfc4d6c1ba0552064ca41d85b825da
Manikandan P <110666644+mani3721@users.noreply.github.com>
2024-06-17 11:38:24 +0530

    Update PostRoute.js

--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { Post, Get, Update, Delete } = require('../Controllers/PostController.js');
+const { Post, Get, Update, Delete, GetOnePost } = require('../Controllers/PostController.js');
 
 
 const router = express.Router();
 
 router.post('/new', Post);
 router.get('/get', Get);
+router.get('/:id', GetOnePost);
 router.put('/update/:id', Update);
 router.delete('/delete/:id', Delete);
 
