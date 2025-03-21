9fd687f2c409ddc81f4db3e6b2ca2987d05b396d
Petchimuthu <petchimuthu.p@zohocorp.com>
2025-03-21 18:46:32 +0530

    change

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PostRoute = require("./Routes/PostRoute.js")
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 dotenv.config();
